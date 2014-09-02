5535153734b63f5b85016ff23c6bde701850718b
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-02 14:52:35 +1000

    wk1

---
+++ wk1.R
@@ -0,0 +1,31 @@
+setwd("C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\Data-Science-Statistical-Inference")
+library(manipulate)
+x <- c(-.5,0,1,1,1.5)
+y <- c(0,0,2,0,0)
+png('triangle.png')
+plot(x,y, lwd=3, frame=F, type='l')
+dev.off()
+pbeta(.75,2,1)
+
+library(UsingR); data(galton)
+png('par_son.png')
+par(mfrow=c(1,2))
+hist(galton$child,col="blue",breaks=100)
+hist(galton$parent,col="blue",breaks=100)
+dev.off()
+
+myHist <- function(mu){
+    hist(galton$child,col="blue",breaks=100)
+    lines(c(mu, mu), c(0, 150),col="red",lwd=5)
+    mse <- mean((galton$child - mu)^2)
+    text(63, 150, paste("mu = ", mu))
+    text(63, 140, paste("Imbalance = ", round(mse, 2)))
+}
+manipulate(myHist(mu), mu = slider(62, 74, step = 0.5))
+
+png('dia_1.png')
+par(mfrow=c(1,1))
+hist(galton$child,col="blue",breaks=100)
+meanChild <- mean(galton$child)
+lines(rep(meanChild,100),seq(0,150,length=100),col="red",lwd=5)
+dev.off()
