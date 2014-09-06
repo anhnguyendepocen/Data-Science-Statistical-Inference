5a1930f014a36d2f44629d49fe6bb010a5bb1dce
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-07 09:52:11 +1000

    wk3

---
+++ wk3_pvalue.R
@@ -0,0 +1,6 @@
+setwd("C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\Data-Science-Statistical-Inference")
+pt(0.8, 15, lower.tail = FALSE)
+
+choose(8, 7) * 0.5^8 + choose(8, 8) * 0.5^8
+pbinom(6, size = 8, prob = 0.5, lower.tail = FALSE)
+ppois(9, 5, lower.tail = FALSE)
